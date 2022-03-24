537429637c747871381d0c16bf0dbf7dcacefc73
Miguel Machado <miiguellmp@gmail.com>
2022-03-24 12:30:15 -0300

    Initial commit from Create Next App

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+
+    html,
+    body,
+    #__next {
+      height: 100%;
+    }
+
+    body {
+      font-family: ${theme.font.family};
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+  `}
+`
+
+export default GlobalStyle
