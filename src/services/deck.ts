994eadd260cdebc8d74503a6006d9e3624f64a65
Miguel Machado <29252011+MiguelMachado-dev@users.noreply.github.com>
2022-03-25 12:02:05 -0300

    feat(service): create deck service api

---
+++ src/services/deck.ts
@@ -0,0 +1,34 @@
+import { api } from '.'
+
+type Card = {
+  image: string
+  value: string
+  suit: string
+  code: string
+}
+
+export type shuffleDeckProps = {
+  success: boolean
+  deck_id: string
+  shuffled: boolean
+  remaining: number
+}
+
+export type initialCardsProps = {
+  success: boolean
+  cards: Array<Card>
+  deck_id: string
+  remaining: boolean
+}
+
+export const shuffleDeck = async (): Promise<shuffleDeckProps> => {
+  const { data } = await api.get('/new/shuffle/?deck_count=1')
+  return data
+}
+
+export const drawInitialCards = async (
+  deckId: string
+): Promise<initialCardsProps> => {
+  const { data } = await api.get(`/${deckId}/draw/?count=5`)
+  return data
+}
