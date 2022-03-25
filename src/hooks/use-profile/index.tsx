c9794de51cfeac5df4f8d8a56dd813a69045e4c0
Miguel Machado <29252011+MiguelMachado-dev@users.noreply.github.com>
2022-03-25 12:01:18 -0300

    feat(hook): create boolean to check if there is any username

--- src/hooks/use-profile/index.tsx
+++ src/hooks/use-profile/index.tsx
@@ -5,11 +5,13 @@ const PROFILE_KEY = 'profile'
 
 export type ProfileContextData = {
   userName: string
+  hasUserName: boolean
   addProfile: (name: string) => void
 }
 
 export const ProfileContextDefaultValues = {
   userName: '',
+  hasUserName: false,
   addProfile: () => null,
 }
 
@@ -23,22 +25,28 @@ export type ProfileProviderProps = {
 
 const ProfileProvider = ({ children }: ProfileProviderProps) => {
   const [userName, setUserName] = useState<string>('')
+  const [hasUserName, setHasUserName] = useState<boolean>(false)
 
   useEffect(() => {
     const data = getStorageItem(PROFILE_KEY)
     if (data) {
       setUserName(data)
+      setHasUserName(true)
+    } else {
+      setHasUserName(false)
     }
   }, [])
 
   const addProfile = (name: string) => {
     setUserName(name)
     setStorageItem(PROFILE_KEY, name)
+    setHasUserName(true)
   }
 
   return (
     <ProfileContext.Provider
       value={{
+        hasUserName,
         userName,
         addProfile,
       }}
