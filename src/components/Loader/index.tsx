636879ee4ec0b395ee498bec4fbaca37abacb76c
Miguel Machado <29252011+MiguelMachado-dev@users.noreply.github.com>
2022-03-24 22:29:03 -0300

    feat(loader): create loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Loader = () => {
+  return (
+    <S.Loader>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </S.Loader>
+  )
+}
+
+export { Loader }
